578855067a6af1e96255f32b9a4430429631bb2a
Shankar Developer <pcred.shankar@gmail.com>
2026-05-26 19:55:50 +0530

    banner changes done

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ import { useState } from "react";
 export default function Home() {
   return (
     <div className="relative w-full">
-      <video
-        src="/banner.webm"
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="w-full object-cover h-screen"
-      />
+      <div className="">
+        <video
+          src="/banner.webm"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full object-cover h-screen"
+        />
+        <div className="bg-white w-full blur-2xl h-70 -mt-18"></div>
+      </div>
     </div>
   );
 }
--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -68,7 +68,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
 
   return (
     <div
-      className={cn("fixed bg-black inset-x-0 top-0 py-1 z-40 w-full", className)}
+      className={cn("fixed bg-white/80 inset-x-0 top-0 py-1 z-40 w-full", className)}
     >{children}    </div>
   );
 };
@@ -118,7 +118,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
         <a
           onMouseEnter={() => setHovered(idx)}
           onClick={onItemClick}
-          className={cn("relative px-4 py-2 rounded-full", hovered === idx ? "text-black" : "text-white")}
+          className={"relative px-4 py-2 rounded-full text-black"}
           key={`link-${idx}`}
           href={item.link}
         >
@@ -195,7 +195,7 @@ export const MobileNavMenu = ({
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           className={cn(
-            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950",
+            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset] dark:bg-neutral-950",
             className,
           )}
         >
